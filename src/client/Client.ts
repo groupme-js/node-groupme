dfd10bf92274391189614fa16d072afc4e3ca406
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-02-17 12:24:50 -0500

    Update Client.ts

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,8 +1,18 @@
 import EventEmitter from "events";
+import GroupManager from "managers/GroupManager";
+import UserManager from "managers/UserManager";
+import RESTManager from "rest/rest";
 
 export default class Client extends EventEmitter {
-    constructor() {
+    groups: GroupManager;
+    users: UserManager;
+    token: string;
+    rest: RESTManager;
+    constructor(token: string) {
         super();
-        
+        this.token = token;
+        this.groups = new GroupManager(this);
+        this.users = new UserManager(this);
+        this.rest = new RESTManager(this);
     }
 }
