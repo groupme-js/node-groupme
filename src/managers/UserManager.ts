f6231e7bd7eed2d1f95a208817f33148fc902570
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-02-18 11:25:58 -0500

    Update UserManager.ts

--- src/managers/UserManager.ts
+++ src/managers/UserManager.ts
@@ -1,5 +1,5 @@
 import Client from "client/Client";
-import User from "structures/User";
+import User, { UserData } from "structures/User";
 import Collection from "util/Collection";
 import BaseManager from "./BaseManager";
 
@@ -12,9 +12,22 @@ export default class UserManager implements BaseManager {
     }
 
     /**
-     * Fetches a user by their GroupMe ID.
+     * Constructs a User with the specified data and stores it in the cache.
+     * If a User with the specified ID already exists, updates the existing
+     * User with the given data.
+     * @returns the created or modified User
      */
-    public fetch(id: string) {
-
+    public add(userData: UserData): User {
+        let user: User;
+        const cachedUser = this.cache.get(userData.id);
+        if (cachedUser) {
+            cachedUser.name = userData.name;
+            cachedUser.avatar = userData.avatar;
+            user = cachedUser;
+        } else {
+            user = new User(userData);
+            this.cache.set(user.id, user);
+        }
+        return user;
     }
 }
