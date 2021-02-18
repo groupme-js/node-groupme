69e3c7350381614082bf6340d53cc7fff44f54e7
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-02-18 11:26:15 -0500

    Create APIRequest.ts

---
+++ src/rest/APIRequest.ts
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+import { Attachment } from "structures/Attachment";
+
+type HTTPMethod = "GET";
+
+
+export default class APIRequest {
+    constructor (method: HTTPMethod) {
+        
+    }
+    
+    make() {
+        
+    }
+}
