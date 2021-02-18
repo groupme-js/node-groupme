d180b5a6c1ad0f04813b010cd9c6d0c281f0fa69
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-02-18 11:26:09 -0500

    Update User.ts

--- src/structures/User.ts
+++ src/structures/User.ts
@@ -1,9 +1,18 @@
+export type UserData = {
+    id: string,
+    name: string,
+    avatar: string,
+}
+
 export default class User {
-    id: string;
+    private readonly _id: string;
+    public get id(): string {
+        return this._id;
+    }
     avatar: string;
-    name?: string;
-    constructor(data: User) {
-        this.id = data.id;
+    name: string;
+    constructor(data: UserData) {
+        this._id = data.id;
         this.avatar = data.avatar;
         this.name = data.name;
     }
