c8014116c73976c15ad5457d09fa5b762c90d939
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-02-18 11:25:29 -0500

    Create Member.ts

---
+++ src/structures/Member.ts
@@ -0,0 +1,45 @@
+import GroupChatChannel from "./GroupChatChannel";
+import User from "./User";
+
+export type MemberData = {
+    user: User;
+    group: GroupChatChannel;
+    memberID: string;
+    nickname: string;
+    muted: boolean;
+    autokicked: boolean;
+    roles: ["admin", "owner"] | ["admin"] | ["user"];
+}
+
+export default class Member {
+    private readonly _user: User;
+    public get user(): User {
+        return this._user;
+    }
+    private readonly _group: GroupChatChannel;
+    public get group(): GroupChatChannel {
+        return this._group;
+    }
+    private readonly _memberID: string;
+    public get memberID(): string {
+        return this._memberID;
+    }
+    nickname: string;
+    muted: boolean;
+    autokicked: boolean;
+    roles: ("admin" | "owner" | "user")[];
+
+    public get admin(): boolean {
+        return this.roles.includes("admin");
+    }
+
+    constructor(data: MemberData) {
+        this._user = data.user;
+        this._group = data.group;
+        this._memberID = data.memberID;
+        this.nickname = data.nickname;
+        this.muted = data.muted;
+        this.autokicked = data.autokicked;
+        this.roles = data.roles;
+    }
+}
