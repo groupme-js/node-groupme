96f3dcb6f49158f121bfb6c320330a8e8d9b6f03
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-02-18 11:25:19 -0500

    Create MemberManager.ts

---
+++ src/managers/MemberManager.ts
@@ -0,0 +1,38 @@
+import Collection from "util/Collection";
+import GroupChatChannel from "structures/GroupChatChannel";
+import BaseManager from "./BaseManager";
+import Member, { MemberData } from "structures/Member";
+import Client from "client/Client";
+
+export default class MemberManager implements BaseManager {
+    client: Client;
+    group: GroupChatChannel;
+    cache: Collection<string, Member>;
+    constructor(client: Client, group: GroupChatChannel) {
+        this.client = client;
+        this.group = group;
+        this.cache = new Collection<string, Member>();
+    }
+
+    /**
+     * Constructs a Member with the specified data and stores it in the cache.
+     * If a Member with the specified ID already exists, updates the existing
+     * Member with the given data.
+     * @returns the created or modified Member
+     */
+    public add(memberData: MemberData): Member {
+        let member: Member;
+        const cachedMember = this.cache.get(memberData.memberID);
+        if (cachedMember) {
+            cachedMember.autokicked = memberData.autokicked;
+            cachedMember.muted = memberData.muted;
+            cachedMember.nickname = memberData.nickname;
+            cachedMember.roles = memberData.roles;
+            member = cachedMember;
+        } else {
+            member = new Member(memberData);
+            this.cache.set(member.memberID, member);
+        }
+        return member;
+    }
+}
