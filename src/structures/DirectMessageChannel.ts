fd7e2da3d35c084814fae4f23e35a45af10bc91a
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-02-18 11:46:23 -0500

    Update DirectMessageChannel.ts

--- src/structures/DirectMessageChannel.ts
+++ src/structures/DirectMessageChannel.ts
@@ -1,21 +1,21 @@
 import Channel from "./Channel";
-import Message from "./Message";
 import User from "./User";
 
-export default class DirectMessageChannel implements Channel {
-    type: "dm" = "dm";
-    id: string;
-    messageCount: number;
-    lastMessage: Message;
-    createdAt: Date;
-    updatedAt: Date;
+export type ChatData = Omit<DirectMessageChannel, "type">
+
+export default class DirectMessageChannel extends Channel {
+    readonly type: "dm" = "dm";
     recipient: User;
-    constructor(data: Omit<DirectMessageChannel, "type">) {
-        this.id = data.id;
-        this.messageCount = data.messageCount;
-        this.lastMessage = data.lastMessage;
-        this.createdAt = data.createdAt;
-        this.updatedAt = data.updatedAt;
+    constructor(data: ChatData) {
+        super({
+            id: data.id,
+            type: "dm",
+            client: data.client,
+            lastMessage: data.lastMessage,
+            messageCount: data.messageCount,
+            createdAt: data.createdAt,
+            updatedAt: data.updatedAt,
+        })
         this.recipient = data.recipient;
     }
 }
