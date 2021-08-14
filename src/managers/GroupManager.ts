1e3b46705980059f128e944b180bc4bf1a87a516
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-08-13 23:44:14 -0400

    Manager overhaul

--- src/managers/GroupManager.ts
+++ src/managers/GroupManager.ts
@@ -12,39 +12,19 @@ export default class GroupManager implements BaseManager {
     }
 
     /**
-     * Constructs a Member with the specified data and stores it in the cache.
-     * If a Member with the specified ID already exists, updates the existing
-     * Member with the given data.
-     * @returns the created or modified Member
+     * Constructs a Group with the specified data and stores it in the cache.
+     * If a Group with the specified ID already exists, updates the existing
+     * Group with the given data.
+     * @returns the created or modified Group
      */
     public add(groupData: GroupData): Group {
-        let group: Group;
         const cachedGroup = this.cache.get(groupData.id);
         if (cachedGroup) {
-            cachedGroup.createdAt = groupData.createdAt;
-            cachedGroup.creatorID = groupData.creatorID;
-            cachedGroup.imageURL = groupData.imageURL;
-            cachedGroup.inviteQR = groupData.inviteQR;
-            cachedGroup.inviteURL = groupData.inviteURL;
-            cachedGroup.joinQuestion = groupData.joinQuestion;
-            cachedGroup.lastMessage = groupData.lastMessage;
-            cachedGroup.likeIcon = groupData.likeIcon;
-            cachedGroup.maxMembers = groupData.maxMembers;
-            cachedGroup.messageCount = groupData.messageCount;
-            cachedGroup.mutedUntil = groupData.mutedUntil;
-            cachedGroup.name = groupData.name;
-            cachedGroup.officeMode = groupData.officeMode;
-            cachedGroup.phoneNumber = groupData.phoneNumber;
-            cachedGroup.private = groupData.private;
-            cachedGroup.requiresApproval = groupData.requiresApproval;
-            cachedGroup.showJoinQuestion = groupData.showJoinQuestion;
-            cachedGroup.theme = groupData.theme;
-            cachedGroup.updatedAt = groupData.updatedAt;
-            group = cachedGroup;
-        } else {
-            group = new Group(groupData);
-            this.cache.set(group.id, group);
+            Object.assign(cachedGroup, groupData);
+            return cachedGroup;
         }
+        const group = new Group(groupData);
+        this.cache.set(group.id, group);
         return group;
     }
 }
