f5460c9abf9e089f4783311588c19021e675c9e7
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-08-14 15:36:21 -0400

    Add documentation

--- src/rest/rest.ts
+++ src/rest/rest.ts
@@ -113,8 +113,36 @@ export default class RESTManager {
         return data.response;
     }
 
-    async fetchGroups() {
-        const groups = await this._api<GroupsIndexResponse>("groups");
+    /**
+     * Fetches groups from the API. 
+     * 
+     * By default, this method fetches all groups that the client is in. 
+     * Use `options.page` and `options.per_page` to specify a paginated section of groups to fetch.
+     * 
+     * @param options Options for fetching groups. All groups are fetched if `page` and `per_page` are omitted.
+     * @returns A Collection of groups that were fetched, or `client.groups.cache` if all groups were fetched.
+     */
+    async fetchGroups(options?: {
+        page?: number,
+        per_page?: number,
+        /** Whether to omit membership data from the response. 
+         * Recommended if dealing with very large groups. Defaults to false. */
+        omit_members?: boolean,
+    }) {
+        const apiParams: GroupsRequestParams = {};
+        if (options) {
+            // If no pagination is specified, recursively fetch all groups
+            if (options.page === undefined && options.per_page === undefined) {
+                let batch, i = 1;
+                do batch = await this.fetchGroups({ page: i++, omit_members: options.omit_members });
+                while (batch.size);
+                return this.client.groups.cache;
+            }
+            // Translate the options into valid API parameters
+            if (options.page != undefined) apiParams.page = options.page;
+            if (options.per_page != undefined) apiParams.per_page = options.per_page;
+            if (options.omit_members == true) apiParams.omit = "memberships";
+        }
 
         groups.forEach(g => {
             /** The Group object to store data in. */
