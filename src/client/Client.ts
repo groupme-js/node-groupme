03e1042427c134729a44dea043e16dab00ed7060
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-08-13 23:43:20 -0400

    Create ChatManager

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,11 +1,13 @@
 import EventEmitter from "events";
 import GroupManager from "../managers/GroupManager";
 import UserManager from "../managers/UserManager";
+import ChatManager from "../managers/ChatManager";
 import RESTManager from "../rest/rest";
 
 export class Client extends EventEmitter {
     groups: GroupManager;
     users: UserManager;
+    chats: ChatManager;
     token: string;
     rest: RESTManager;
     constructor(token: string) {
@@ -13,6 +15,7 @@ export class Client extends EventEmitter {
         this.token = token;
         this.groups = new GroupManager(this);
         this.users = new UserManager(this);
+        this.chats = new ChatManager(this);
         this.rest = new RESTManager(this);
     }
 }
---
+++ src/managers/ChatManager.ts
@@ -0,0 +1,30 @@
+import { Client } from "../client/Client";
+import { Chat, ChatData } from "../structures/Chat";
+import Collection from "../util/Collection";
+import BaseManager from "./BaseManager";
+
+export default class ChatManager implements BaseManager {
+    client: Client;
+    cache: Collection<string, Chat>;
+    constructor(client: Client) {
+        this.client = client;
+        this.cache = new Collection<string, Chat>();
+    }
+
+    /**
+     * Constructs a Chat with the specified data and stores it in the cache.
+     * If a Chat with the specified ID already exists, updates the existing
+     * Chat with the given data.
+     * @returns the created or modified Chat
+     */
+    public add(chatData: ChatData): Chat {
+        const cachedChat = this.cache.get(chatData.id);
+        if (cachedChat) {
+            Object.assign(cachedChat, chatData);
+            return cachedChat;
+        }
+        const chat = new Chat(chatData);
+        this.cache.set(chat.id, chat);
+        return chat;
+    }
+}
