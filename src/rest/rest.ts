b8436e1edb896112432c6fa269f8805181be998b
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-08-13 23:44:03 -0400

    Implement REST interface

--- src/rest/rest.ts
+++ src/rest/rest.ts
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import { Client } from "../client/Client";
 import { Attachment } from "../structures/Attachment";
+import { User } from "../structures/User";
 
 type GroupMeAPIResponse<T> = {
     response: T,
@@ -8,7 +9,7 @@ type GroupMeAPIResponse<T> = {
         code: number,
         errors: any[]
     }
-}
+};
 
 type GroupsIndexResponse = {
     id: string,
@@ -60,18 +61,34 @@ type GroupsIndexResponse = {
     join_question: null // just pretend that this will never exist :clueless:
 }[];
 
-async function api<T>(path: string): Promise<T> {
-    const url = "https://api.groupme.com/v3" + path;
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-    const data = await (response.json() as Promise<GroupMeAPIResponse<T>>);
-    if (data.meta.errors.length > 0) {
-        throw new Error(data.meta.errors.join('; '));
-    }
-    return data.response;
-}
+type ChatsIndexResponse = {
+    created_at: number,
+    updated_at: number,
+    messages_count: number,
+    message_deletion_period: number,
+    message_deletion_mode: string[],
+    last_message: {
+        attachments: Attachment[],
+        avatar_url: string,
+        conversation_id: string,
+        created_at: number,
+        favorited_by: [], // This seems to always be empty
+        id: string,
+        name: string,
+        recipient_id: string,
+        sender_id: string,
+        sender_type: string,
+        source_guid: string,
+        text: string,
+        user_id: string,
+    },
+    other_user: {
+        avatar_url: string,
+        id: string,
+        name: string,
+    },
+}[];
+
 
 export default class RESTManager {
     client: Client;
@@ -79,70 +96,118 @@ export default class RESTManager {
         this.client = client;
     }
 
-    fetchGroups() {
-        return api<GroupsIndexResponse>("groups").then(groups => {
-            groups.forEach(g => {
-                /** The Group object to store data in. */
-                const group = this.client.groups.add({
-                    client: this.client,
-                    createdAt: g.created_at,
-                    creatorID: g.creator_user_id,
-                    id: g.id,
-                    imageURL: g.image_url,
-                    inviteQR: g.share_qr_code_url,
-                    inviteURL: g.share_url,
-                    joinQuestion: g.join_question,
-                    lastMessage: {
-                        id: g.messages.last_message_id,
-                        createdAt: g.messages.last_message_created_at,
-                        text: g.messages.preview.text,
-                        attachments: g.messages.preview.attachments,
-                        user: {
-                            image_url: g.messages.preview.image_url,
-                            nickname: g.messages.preview.nickname,
-                        }
-                    },
-                    likeIcon: g.like_icon,
-                    maxMembers: g.max_members,
-                    messageCount: g.messages.count,
-                    mutedUntil: g.muted_until,
-                    name: g.name,
-                    officeMode: g.office_mode,
-                    phoneNumber: g.phone_number,
-                    private: g.type == "private",
-                    requiresApproval: g.requires_approval,
-                    showJoinQuestion: g.show_join_question,
-                    theme: g.theme_name,
-                    updatedAt: g.updated_at,
-                    messageDeletionMode: g.message_deletion_mode,
-                    messageDeletionPeriod: g.message_deletion_period,
-                });
-                if (g.members) {
-                    g.members.forEach(m => {
-                        const user = this.client.users.add({
-                            id: m.user_id,
-                            avatar: m.image_url,
-                            name: m.name,
-                        });
-                        const memberData = {
-                            group: group,
-                            user: user,
-                            memberID: m.id,
-                            nickname: m.nickname,
-                            autokicked: m.autokicked,
-                            muted: m.muted,
-                            roles: m.roles,
-                        };
-                        group.members.add(memberData);
-                    });
-                }
-
-            });
-            return this.client.groups.cache;
+    async _api<T>(path: string): Promise<T> {
+        const url = "https://api.groupme.com/v3/" + path;
+        const response = await fetch(url, {
+            headers: { 'X-Access-Token': this.client.token }
         });
+        // console.log(`-----\nAPI request\nurl: ${url}\n`, response, `\n-----`)
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const data = await (response.json() as Promise<GroupMeAPIResponse<T>>);
+        // console.log(data);
+        if (data.meta.errors) {
+            throw new Error(data.meta.errors.join('; '));
+        }
+        return data.response;
     }
 
-    fetchChats() {
+    async fetchGroups() {
+        const groups = await this._api<GroupsIndexResponse>("groups");
 
+        groups.forEach(g => {
+            /** The Group object to store data in. */
+            const group = this.client.groups.add({
+                client: this.client,
+                createdAt: g.created_at,
+                creatorID: g.creator_user_id,
+                id: g.id,
+                imageURL: g.image_url,
+                inviteQR: g.share_qr_code_url,
+                inviteURL: g.share_url,
+                joinQuestion: g.join_question,
+                lastMessage: {
+                    id: g.messages.last_message_id,
+                    createdAt: g.messages.last_message_created_at,
+                    text: g.messages.preview.text,
+                    attachments: g.messages.preview.attachments,
+                    user: {
+                        image_url: g.messages.preview.image_url,
+                        nickname: g.messages.preview.nickname,
+                    }
+                },
+                likeIcon: g.like_icon,
+                maxMembers: g.max_members,
+                messageCount: g.messages.count,
+                mutedUntil: g.muted_until,
+                name: g.name,
+                officeMode: g.office_mode,
+                phoneNumber: g.phone_number,
+                private: g.type == "private",
+                requiresApproval: g.requires_approval,
+                showJoinQuestion: g.show_join_question,
+                theme: g.theme_name,
+                updatedAt: g.updated_at,
+                messageDeletionMode: g.message_deletion_mode,
+                messageDeletionPeriod: g.message_deletion_period,
+            });
+
+            if (g.members) {
+                g.members.forEach(m => {
+                    const user = this.client.users.add({
+                        id: m.user_id,
+                        avatar: m.image_url,
+                        name: m.name,
+                    });
+                    const memberData = {
+                        group: group,
+                        user: user,
+                        memberID: m.id,
+                        nickname: m.nickname,
+                        autokicked: m.autokicked,
+                        muted: m.muted,
+                        roles: m.roles,
+                    };
+                    group.members.add(memberData);
+                });
+            }
+
+        });
+
+        return this.client.groups.cache;
+    }
+
+    async fetchChats() {
+        const chats = await this._api<ChatsIndexResponse>("chats");
+
+        chats.forEach(c => {
+            this.client.chats.add({
+                client: this.client,
+                createdAt: c.created_at,
+                updatedAt: c.updated_at,
+                id: c.last_message.conversation_id,
+                recipient: this.client.users.add({
+                    id: c.other_user.id,
+                    name: c.other_user.name,
+                    avatar: c.other_user.avatar_url,
+                }),
+                lastMessage: {
+                    id: c.last_message.id,
+                    attachments: c.last_message.attachments,
+                    createdAt: c.last_message.created_at,
+                    text: c.last_message.text,
+                    user: {
+                        nickname: c.last_message.name,
+                        image_url: c.last_message.avatar_url,
+                    },
+                },
+                messageCount: c.messages_count,
+                messageDeletionMode: c.message_deletion_mode,
+                messageDeletionPeriod: c.message_deletion_period,
+            });
+        });
+
+        return this.client.chats.cache;
     }
 }
