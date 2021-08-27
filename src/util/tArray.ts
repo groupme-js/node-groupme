da3052ad98f4cb84832c4514b205825af8ce892e
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-08-27 10:52:29 -0400

    Create tArray.ts

---
+++ src/util/tArray.ts
@@ -0,0 +1,10 @@
+import { AssertionError } from "assert";
+
+export default function tArray<T>(transformer: (json: any) => T): (array: any) => T[] {
+    return (obj: any): T[] => {
+        if (!Array.isArray(obj)) throw new AssertionError({ message: 'Expected object to be an array.', actual: obj, expected: 'T[]' });
+        const arr: T[] = [];
+        obj.forEach(value => arr.push(transformer(value)));
+        return arr;
+    }
+}
