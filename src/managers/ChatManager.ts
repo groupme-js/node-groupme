a002c5e5026521834cfcdcb92e823f0146de3fd8
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-08-30 14:22:49 -0400

    Move fetchChats to ChatManager class

--- src/managers/ChatManager.ts
+++ src/managers/ChatManager.ts
@@ -1,6 +1,12 @@
 import { Client, Collection } from "..";
+import { ChatsIndexResponse, toChats } from "../interfaces"
 import { Chat, ChatData } from "../structures/Chat";
 import BaseManager from "./BaseManager";
+
+type ChatsRequestParams = {
+    page?: number,
+    per_page?: number
+}
 
 export default class ChatManager implements BaseManager {
     client: Client;
@@ -25,5 +31,57 @@ export default class ChatManager implements BaseManager {
         const chat = new Chat(chatData);
         this.cache.set(chat.id, chat);
         return chat;
+    }
+
+    async fetchChats(options?: {
+        page?: number,
+        per_page?: number,
+    }) {
+        const apiParams: ChatsRequestParams = {};
+        if (options) {
+            // If no pagination is specified, recursively fetch all chats
+            if (options.page === undefined && options.per_page === undefined) {
+                let batch, i = 1;
+                do batch = await this.fetchChats({ page: i++ });
+                while (batch.size);
+                return this.client.chats.cache;
+            }
+            // Translate the options into valid API parameters
+            if (options.page != undefined) apiParams.page = options.page;
+            if (options.per_page != undefined) apiParams.per_page = options.per_page;
+        }
+
+        const batch = new Collection<string, Chat>()
+        const chats = await this.client.rest.api<ChatsIndexResponse[]>("GET", "chats", toChats, { query: apiParams });
+
+        chats.forEach(c => {
+            const chat = this.client.chats.add({
+                client: this.client,
+                createdAt: c.created_at,
+                updatedAt: c.updated_at,
+                id: c.other_user.id,
+                conversation_id: c.last_message.conversation_id,
+                recipient: this.client.users.add({
+                    id: c.other_user.id,
+                    name: c.other_user.name,
+                    avatar: c.other_user.avatar_url,
+                }),
+                lastMessage: {
+                    id: c.last_message.id,
+                    attachments: c.last_message.attachments,
+                    createdAt: c.last_message.created_at,
+                    text: c.last_message.text,
+                    user: {
+                        nickname: c.last_message.name,
+                        image_url: c.last_message.avatar_url,
+                    },
+                },
+                messageCount: c.messages_count,
+                messageDeletionMode: c.message_deletion_mode,
+                messageDeletionPeriod: c.message_deletion_period,
+            });
+            batch.set(chat.recipient.id, chat);
+        });
+        return batch;
     }
 }
