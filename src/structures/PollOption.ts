193e836b6c915a16006fbef38af848396fea91c7
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-09-15 21:20:28 -0400

    Create PollOption.ts

---
+++ src/structures/PollOption.ts
@@ -0,0 +1,24 @@
+import { Collection, User, Poll } from "..";
+import { PollOptionData } from "../interfaces";
+
+interface PollOptionInterface {
+    vote(): Promise<this>
+}
+
+export default class PollOption implements PollOptionInterface {
+    readonly poll: Poll;
+    id: string;
+    title: string;
+    votes: number
+    voters: Collection<string, User>
+    constructor(poll: Poll, data: PollOptionData) {
+        this.poll = poll;
+        this.id = data.id;
+        this.title = data.title;
+        this.votes = data.votes ? data.votes : 0;
+        this.voters = new Collection<string, User>();
+    }
+    vote(): Promise<this> {
+        throw new Error("Method not implemented.");
+    }
+}
