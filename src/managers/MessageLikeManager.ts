2176075daa00b33a1bebb8dc5612de059a1e6446
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-09-15 21:20:12 -0400

    Create Poll class

---
+++ src/managers/MessageLikeManager.ts
@@ -0,0 +1,13 @@
+import UserManager from "./UserManager";
+
+interface MessageLikeManagerInterface {
+    get mine(): boolean
+}
+
+export default class MessageLikeManager extends UserManager implements MessageLikeManagerInterface {
+
+    public get mine(): boolean {
+        return this.cache.has(this.client.user.id)
+    }
+
+}
---
+++ src/structures/Poll.ts
@@ -0,0 +1,73 @@
+import { Client, Collection, Group, User } from "..";
+import { APIPoll } from "../interfaces";
+import PollOption from "./PollOption";
+
+interface PollInterface {
+    fetch(): Promise<this>
+    vote(option: OptionID | PollOption): Promise<this>
+    vote(options: (OptionID | PollOption)[]): Promise<this>
+    end(): Promise<this>
+    get canEnd(): boolean
+}
+
+type UserID = string
+type OptionID = string
+
+export default class Poll implements PollInterface {
+    readonly client: Client;
+    private _active: boolean;
+    id: string;
+    question: string;
+    options: Collection<OptionID, PollOption>;
+    voters?: Collection<UserID, OptionID> | Collection<UserID, OptionID[]>;
+    myVote?: OptionID | OptionID[];
+    group: Group;
+    creator: User;
+    multi: boolean;
+    public: boolean;
+    createdAt: number;
+    updatedAt: number;
+    expiresAt: number;
+
+    constructor(client: Client, group: Group, creator: User, data: APIPoll) {
+        this.client = client;
+        this.id = data.data.id;
+        this.question = data.data.subject;
+        this.group = group;
+        this.creator = creator;
+        this._active = data.data.status == "active";
+        this.multi = data.data.type == "multi";
+        this.public = data.data.visibility == "public";
+        this.createdAt = data.data.created_at;
+        this.updatedAt = data.data.last_modified;
+        this.expiresAt = data.data.expiration;
+        this.options = new Collection<OptionID, PollOption>();
+        this.voters = this.public ? (
+            this.multi ?
+                new Collection<UserID, OptionID[]>()
+                :
+                new Collection<UserID, OptionID>()
+        ) : undefined;
+        this.myVote = this.multi ? data.user_votes : data.user_vote;
+
+    }
+    vote(option: string | PollOption): Promise<this>;
+    vote(options: (string | PollOption)[]): Promise<this>;
+    vote(options: string | PollOption | (string | PollOption)[]): Promise<this> {
+        throw new Error("Method not implemented.");
+    }
+    fetch(): Promise<this> {
+        throw new Error("Method not implemented.");
+    }
+    end(): Promise<this> {
+        throw new Error("Method not implemented.");
+    }
+    get canEnd(): boolean {
+        throw new Error("Method not implemented.");
+    }
+    public get active(): boolean {
+        throw new Error("Method not implemented.");
+        if (this._active /* && (this.expiresAt has passed) */) this._active = false;
+        return this._active;
+    }
+}
