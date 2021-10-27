6d82f190d10ebe14aea69b0ac9daeee7e5d0de04
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-10-27 11:49:08 -0400

    [fix] add definite type assertion to g.members

--- src/managers/FormerGroupManager.ts
+++ src/managers/FormerGroupManager.ts
@@ -20,7 +20,10 @@ export default class FormerGroupManager extends BaseManager<FormerGroup> impleme
         groupsFormerResponse.forEach(g => {
             /** The Group object to store data in. */
             const formerGroup = this._upsert(new FormerGroup(this.client, g));
-            g.members.forEach(data => {
+
+            // we know that g.members is always defined for former groups
+            // however, it would be nice if the types reflected that...
+            g.members!.forEach(data => {
                 const user = this.client.users._upsert(new User({
                     id: data.user_id,
                     avatar: data.image_url,
