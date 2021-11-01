700caabb8fca588f337385d0493bc16678f005be
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-11-01 15:40:34 -0400

    [feat] group message fetch boilerplate

--- src/managers/GroupMessageManager.ts
+++ src/managers/GroupMessageManager.ts
@@ -1,12 +1,27 @@
 import type { Client, Collection, Group } from "..";
 import type GroupMessage from "../structures/GroupMessage";
-import MessageManager from "./MessageManager";
+import MessageManager, { MessageRequestParams } from "./MessageManager";
 
-export default class GroupMessageManager extends MessageManager<Group, GroupMessage> {
+interface GroupMessageManagerInterface {
+    client: Client
+    channel: Group
+    cache: Collection<string, GroupMessage>
+    fetch(): Promise<Collection<string, GroupMessage>>
+    fetch(id: string): Promise<GroupMessage>
+    fetch(ids: string[]): Promise<Collection<string, GroupMessage>>
+    fetch(options: MessageRequestParams): Promise<Collection<string, GroupMessage>>
+}
+
+export default class GroupMessageManager extends MessageManager<Group, GroupMessage> implements GroupMessageManagerInterface {
     constructor(client: Client, channel: Group) {
         super(client, channel);
     }
-    fetch(): Promise<Collection<string, GroupMessage>> {
+    
+    fetch(): Promise<Collection<string, GroupMessage>>;
+    fetch(id: string): Promise<GroupMessage>;
+    fetch(ids: string[]): Promise<Collection<string, GroupMessage>>;
+    fetch(options: MessageRequestParams): Promise<Collection<string, GroupMessage>>;
+    fetch(options?: any): Promise<Collection<string, GroupMessage>> | Promise<GroupMessage> {
         throw new Error("Method not implemented.");
     }
 }
--- src/managers/MessageManager.ts
+++ src/managers/MessageManager.ts
@@ -1,15 +1,29 @@
 import type { Channel, Client, Collection, Message } from "..";
 import BaseManager from "./BaseManager";
 
-interface MessageManagerInterface {
-    fetch(): Promise<Collection<string, Message>>
+export type MessageRequestParams = {
+    before_id?: string,
+    since_id?: string,
+    after_id?: string,
+    limit?: number,
 }
 
-export default abstract class MessageManager<T extends Channel, U extends Message> extends BaseManager<U> implements MessageManagerInterface {
+interface MessageManagerInterface<T extends Message> {
+    fetch(): Promise<Collection<string, T>>
+    fetch(id: string): Promise<T>
+    fetch(ids: string[]): Promise<Collection<string, T>>
+    fetch(options: MessageRequestParams): Promise<Collection<string, T>>
+}
+
+export default abstract class MessageManager<T extends Channel, U extends Message> extends BaseManager<U> implements MessageManagerInterface<U> {
     readonly channel: T;
     constructor(client: Client, channel: T) {
         super(client);
         this.channel = channel;
     }
-    abstract fetch(): Promise<Collection<string, U>>
+
+    abstract fetch(): Promise<Collection<string, U>>;
+    abstract fetch(id: string): Promise<U>;
+    abstract fetch(ids: string[]): Promise<Collection<string, U>>;
+    abstract fetch(options: MessageRequestParams): Promise<Collection<string, U>>;
 }
