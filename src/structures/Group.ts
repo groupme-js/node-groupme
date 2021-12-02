5636ad5ebf06cca9371001cf04a425ff1a126ad3
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-12-02 13:35:27 -0500

    [feat] implement Group#send(text: string)
    still need overloads

--- src/structures/Group.ts
+++ src/structures/Group.ts
@@ -1,10 +1,10 @@
-import type { APIGroup } from "groupme-api-types";
+import type { APIGroup, PostGroupMessageBody, PostGroupMessageResponse } from "groupme-api-types";
 import type { Client, FormerGroup, Member, Message, SendableChannelInterface } from "..";
 import GroupMessageManager from "../managers/GroupMessageManager";
 import PollManager from "../managers/PollManager";
 import BaseGroup from "./BaseGroup";
 import { ChannelType } from "./Channel";
-import type GroupMessage from "./GroupMessage";
+import GroupMessage from "./GroupMessage";
 
 type GroupUpdateOptions = {
     name: string
@@ -39,42 +39,68 @@ export default class Group extends BaseGroup implements ActiveGroupInterface, Se
         this.messages = new GroupMessageManager(client, this);
         this.polls = new PollManager(client, this);
     }
-    send(message: Message): Promise<GroupMessage> {
-        throw new Error("Method not implemented.");
+
+    public async send(text: string): Promise<GroupMessage> {
+        const body: PostGroupMessageBody = {
+            message: {
+                text,
+                attachments: [],
+                source_guid: this.client.rest.guid(),
+            }
+        };
+        const response = await this.client.rest.api<PostGroupMessageResponse>(
+            'POST',
+            `groups/${this.id}/messages`,
+            { body }
+        );
+        const message = new GroupMessage(this.client, this, response.message);
+        return this.messages._upsert(message);
     }
+
     fetch(): Promise<this> {
         throw new Error("Method not implemented.");
     }
+
     update(options: GroupUpdateOptions): Promise<this> {
         throw new Error("Method not implemented.");
     }
+
     transferOwnershipTo(newOwner: string): Promise<this> {
         throw new Error("Method not implemented.");
     }
+
     delete(): Promise<void> {
         throw new Error("Method not implemented.");
     }
+
     changeNickname(nickname: string): Promise<Member> {
         throw new Error("Method not implemented.");
     }
+
     leave(): Promise<FormerGroup> {
         throw new Error("Method not implemented.");
     }
+
     get canLeave(): boolean {
         throw new Error("Method not implemented.");
     }
+
     get canUpdate(): boolean {
         throw new Error("Method not implemented.");
     }
+
     get canDelete(): boolean {
         throw new Error("Method not implemented.");
     }
+
     get canTransfer(): boolean {
         throw new Error("Method not implemented.");
     }
+
     get canAddMembers(): boolean {
         throw new Error("Method not implemented.");
     }
+
     get canRemoveMembers(): boolean {
         throw new Error("Method not implemented.");
     }
