810dcee71f9269a2fbe853cee8a827ea28bffa7b
Shadow <thebotoftheshadow@gmail.com>
2021-12-21 00:36:08 -0600

    why won't you work

--- tests/index.js
+++ tests/index.js
@@ -1,21 +1,24 @@
 const token = process.env.TOKEN
 
-const expect = require("expect.js")
+const expect = require("chai").expect
+
 const GroupMe = require("..")
 const client = new GroupMe.Client(token)
 
 const data = {
     testGroup: "70077952",
 }
-client.login()
 
-describe("GroupManager", async () => {
+describe("ws", async () => {
+    before(() => {
+        client.login()
+    })
+    after(() => {
+        client.logout()
+    })
+
     let g = await client.groups.fetch(data.testGroup)
-    it("should fetch a group", async () => {
+    it("should fetch a group",  () => {
         expect(g.id).to.be(data.testGroup)
     })
 })
-
-setTimeout(() => {
-    client.logout()
-}, 15000)
