1dbc27ee78872aa43a9dca44ca7276e35e765b98
Shadow <thebotoftheshadow@gmail.com>
2022-01-18 11:24:39 -0600

    Add tests

--- tests/hooks.js
+++ tests/hooks.js
@@ -4,7 +4,9 @@ const { client, data } = require(".")
 exports.mochaHooks = {
     beforeall(done) {
         client.login()
+        console.log(client)
         done()
+        
     },
     afterAll(done) {
         client.logout()
--- tests/index.js
+++ tests/index.js
@@ -3,8 +3,12 @@ const token = process.env.GROUPME_TOKEN
 
 const GroupMe = require("..")
 const client = new GroupMe.Client(token)
+const package = require("../package.json")
+
 const data = {
     testGroup: "70077952",
+    version: package.version,
+    testUser: "99172139"
 }
 
 module.exports = {client, data}
