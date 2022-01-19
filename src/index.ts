0d1b5c7bf1d3a3e980c21a7594f74bff00341e8c
Peter <43360094+not-so-smart@users.noreply.github.com>
2022-01-18 23:50:56 -0500

    [ref] export EVERYTHING
    i had to draw up a whole dependency graph to figure out what order to export everything without causing circular import issues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-export { default as Message } from "./structures/Message";
-export { default as Client } from "./client/Client";
-export { default as Channel, ChannelType, SendableChannelInterface } from "./structures/Channel";
-export { default as Chat } from "./structures/Chat";
-export { default as Member } from "./structures/Member";
-export { default as FormerGroup } from "./structures/FormerGroup";
-export { default as Group } from "./structures/Group";
-export { default as Poll } from "./structures/Poll";
-export { default as User } from "./structures/User";
 export { Collection } from "./util/Collection";
+
+export { default as User } from "./structures/User";
+export { default as Message } from "./structures/Message";
+export { default as BaseManager } from "./managers/BaseManager";
+export { default as UserManager } from "./managers/UserManager";
+export { default as MessageLikeManager } from "./managers/MessageLikeManager";
+export { default as Member } from "./structures/Member";
+export { default as FormerMember } from "./structures/FormerMember";
+export { default as FormerMemberManager } from "./managers/FormerMemberManager";
+export { default as MemberManager } from "./managers/MemberManager";
+export { default as Channel, ChannelType, SendableChannelInterface } from "./structures/Channel";
+export { default as BaseGroup } from "./structures/BaseGroup";
+export { default as Poll } from "./structures/Poll";
+export { default as GroupMessage } from "./structures/GroupMessage";
+export { default as MessageManager } from "./managers/MessageManager";
+export { default as ChatMessage } from "./structures/ChatMessage";
+export { default as ChatMessageManager } from "./managers/ChatMessageManager";
+export { default as GroupMessageManager } from "./managers/GroupMessageManager";
+export { default as PollManager } from "./managers/PollManager";
+export { default as FormerGroup } from "./structures/FormerGroup";
+export { default as Chat } from "./structures/Chat";
+export { default as Group } from "./structures/Group";
+export { default as FormerGroupManager } from "./managers/FormerGroupManager";
+export { default as ChatManager } from "./managers/ChatManager";
+export { default as GroupManager } from "./managers/GroupManager";
+
+export { default as Attachment } from "./structures/Attachment";
+export { default as PollOption } from "./structures/PollOption";
+
+export { default as Client } from "./client/Client";
