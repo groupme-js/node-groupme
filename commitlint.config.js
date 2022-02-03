1ca1c2f2228f73e110c919f00fb937a0362ff7ef
Peter <43360094+not-so-smart@users.noreply.github.com>
2022-02-03 15:52:30 -0500

    ci: :green_heart: disable body-max-line-length rule for commitlint
    this rule is super annoying, plus it completely breaks semantic-release so now we have an excuse to get rid of it. P.S. this line is super long so we'll know that the rule is properly disabled if this commit goes through

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
-module.exports = { 
-    extends: ["@commitlint/config-conventional"],
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
     rules: {
-        "subject-case": [1, "always", ["lower-case"]],
-    }
+        'subject-case': [1, 'always', ['lower-case']],
+        'body-max-line-length': [0],
+    },
 }
