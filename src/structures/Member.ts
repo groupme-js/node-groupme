3fe62e87ce34f6b18813fca6fd2dc3b256863027
Peter <43360094+not-so-smart@users.noreply.github.com>
2022-05-22 21:51:13 -0400

    refactor(Member): :building_construction: extend `Base`

--- src/structures/Member.ts
+++ src/structures/Member.ts
@@ -1,22 +1,20 @@
 import type { APIMember } from 'groupme-api-types'
 import type { BaseGroup, Client, User } from '..'
+import { Base } from '..'
 
 interface MemberInterface {}
 
-export default class Member implements MemberInterface {
+export default class Member extends Base implements MemberInterface {
     readonly user: User
     readonly group: BaseGroup
     readonly memberID: string
-    readonly client: Client
-    readonly id: string
     nickname: string
     muted: boolean
     // autokicked: boolean
     roles: ('admin' | 'owner' | 'user')[]
 
     constructor(client: Client, group: BaseGroup, user: User, data: APIMember) {
-        this.client = client
-        this.id = user.id
+        super(client, user.id)
         this.user = user
         this.group = group
         this.memberID = data.id
@@ -24,6 +22,20 @@ export default class Member implements MemberInterface {
         this.muted = data.muted
         // this.autokicked = data.autokicked
         this.roles = data.roles
+    }
+
+    _patch(data: Partial<APIMember>): this {
+        this.user._patch({
+            name: data.name,
+            // don't bother updating the user avatar because we don't know whether it's real or not
+        })
+
+        if (data.nickname !== undefined) this.nickname = data.nickname
+        if (data.image_url !== undefined) this.image_url = data.image_url
+        if (data.muted !== undefined) this.muted = data.muted
+        if (data.roles !== undefined) this.roles = data.roles
+
+        return this
     }
 
     get isAdmin(): boolean {
