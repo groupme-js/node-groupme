e5feacbd0e914281c264200b75d44eee08b747f6
Peter <43360094+not-so-smart@users.noreply.github.com>
2022-05-22 21:12:12 -0400

    refactor(Channel): :building_construction: extend `Base`, update constructor signature
    first argument is now `client` to match other subclasses of `Base`
    
    BREAKING CHANGE: Channel constructor changed:
    `Channel(ChannelData)` -> `Channel(Client, ChannelData)`

--- src/structures/Channel.ts
+++ src/structures/Channel.ts
@@ -1,4 +1,5 @@
 import type { Attachment, Client, Message, MessageManager } from '..'
+import { Base } from '..'
 
 export enum ChannelType {
     Chat = 'chat',
@@ -21,7 +22,6 @@ type UserPreview = {
 
 interface ChannelData {
     readonly id: string
-    readonly client: Client
     messageCount: number
     lastMessage: MessagePreview
     createdAt: number
@@ -30,25 +30,33 @@ interface ChannelData {
     messageDeletionPeriod?: number
 }
 
-export default abstract class Channel implements ChannelData {
+export default abstract class Channel extends Base implements ChannelData {
     abstract readonly type: ChannelType
-    readonly id: string
-    readonly client: Client
     messageCount: number
     lastMessage: MessagePreview
     createdAt: number
     updatedAt: number
     messageDeletionMode?: string[]
     messageDeletionPeriod?: number
-    constructor(data: ChannelData) {
-        this.id = data.id
-        this.client = data.client
+    constructor(client: Client, data: ChannelData) {
+        super(client, data.id)
         this.messageCount = data.messageCount
         this.lastMessage = data.lastMessage
         this.createdAt = data.createdAt
         this.updatedAt = data.updatedAt
         this.messageDeletionMode = data.messageDeletionMode
         this.messageDeletionPeriod = data.messageDeletionPeriod
+    }
+
+    static _patch(self: Channel, data: Partial<ChannelData>): typeof self {
+        if (data.messageCount !== undefined) self.messageCount = data.messageCount
+        if (data.lastMessage !== undefined) self.lastMessage = data.lastMessage
+        if (data.createdAt !== undefined) self.createdAt = data.createdAt
+        if (data.updatedAt !== undefined) self.updatedAt = data.updatedAt
+        if (data.messageDeletionMode !== undefined) self.messageDeletionMode = data.messageDeletionMode
+        if (data.messageDeletionPeriod !== undefined) self.messageDeletionPeriod = data.messageDeletionPeriod
+
+        return self
     }
 }
 
