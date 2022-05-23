7641639f7409c2f9cf56ac857688e8b99c6a7d0b
Peter <43360094+not-so-smart@users.noreply.github.com>
2022-05-22 21:44:00 -0400

    feat(Base): :poop: implement BaseGroup#_patch
    todo: fix the unsafe type assertion

--- src/structures/BaseGroup.ts
+++ src/structures/BaseGroup.ts
@@ -48,4 +48,32 @@ export default abstract class BaseGroup extends Channel {
         this.showJoinQuestion = data.show_join_question
         this.joinQuestion = data.join_question ? data.join_question.text : null
     }
+    _patch(data: Partial<APIGroup>): this {
+        Channel._patch(this, Channel.dataFromGroup(data as APIGroup)) // this is dangerous
+
+        if (data.name !== undefined) this.name = data.name
+        if (data.phone_number !== undefined) this.phoneNumber = data.phone_number
+        if (data.type !== undefined) this.closed = data.type == 'closed'
+        if (data.image_url !== undefined) this.imageURL = data.image_url
+        if (data.creator_user_id !== undefined) this.creatorID = data.creator_user_id
+        if (data.muted_until !== undefined) this.mutedUntil = data.muted_until
+        if (data.office_mode !== undefined) this.officeMode = data.office_mode
+        if (data.share_url !== undefined) this.inviteURL = data.share_url
+        if (data.share_qr_code_url !== undefined) this.inviteQR = data.share_qr_code_url
+        if (data.max_members !== undefined) this.maxMembers = data.max_members
+        if (data.theme_name !== undefined) this.theme = data.theme_name
+        if (data.like_icon !== undefined)
+            this.likeIcon = data.like_icon
+                ? {
+                      packId: data.like_icon.pack_id,
+                      packIndex: data.like_icon.pack_index,
+                      type: 'emoji',
+                  }
+                : null
+        if (data.requires_approval !== undefined) this.requiresApproval = data.requires_approval
+        if (data.show_join_question !== undefined) this.showJoinQuestion = data.show_join_question
+        if (data.join_question !== undefined) this.joinQuestion = data.join_question ? data.join_question.text : null
+
+        return this
+    }
 }
