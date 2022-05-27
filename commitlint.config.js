dc1a63a7c908cdf0d974928424fc8c0a361021a8
Shadow <thebotoftheshadow@gmail.com>
2022-05-27 13:37:33 -0500

    ci: New limits

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,8 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'subject-case': [1, 'always', ['lower-case']],
+        'body-max-line-length': [500],
+        'footer-max-line-length': [500],
         'footer-leading-blank': 'never'
     },
 }
