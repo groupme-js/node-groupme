e9df382758bcc024a3477b107739c11c0e75b595
Shadow <thebotoftheshadow@gmail.com>
2022-05-27 13:18:48 -0500

    meta: Remove the body length requirement

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,5 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'subject-case': [1, 'always', ['lower-case']],
-        'body-max-line-length': [0],
     },
 }
