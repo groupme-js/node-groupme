68b9a46506d731b7f9c0457633e2f6647ccf6d8c
Peter <43360094+not-so-smart@users.noreply.github.com>
2022-05-27 14:46:27 -0400

    ci: :green_heart: disable some commitlint rules
    footer-max-line-length and footer-leading-blank were breaking semantic-release again so now they're gone

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,7 @@ module.exports = {
     rules: {
         'subject-case': [1, 'always', ['lower-case']],
         'body-max-line-length': [0],
+        'footer-max-line-length': [0],
+        'footer-leading-blank': [0],
     },
 }
