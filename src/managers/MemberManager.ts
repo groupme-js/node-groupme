93222a18a854081c04a8c588524ed82f0c11b1b0
Darren Kam <darrenmkam4150@gmail.com>
2022-06-03 11:42:10 -1000

    feat(Members): implement member removal

--- src/managers/MemberManager.ts
+++ src/managers/MemberManager.ts
@@ -1,10 +1,11 @@
+import type { PostMemberRemoveResponse } from 'groupme-api-types'
 import type { BaseGroup, Client, Collection } from '..'
-import { BaseManager, FormerMemberManager, Member } from '..'
+import { BaseManager, FormerMember, FormerMemberManager, FormerMemberState, Member } from '..'
 
 interface MemberManagerInterface {
     add(id: string): Promise<Member>
     add(ids: string[]): Promise<Collection<string, Member>>
-    remove(member: Member): Promise<this>
+    remove(member: Member): Promise<FormerMember>
 }
 
 export default class MemberManager extends BaseManager<Member, typeof Member> implements MemberManagerInterface {
@@ -21,7 +22,14 @@ export default class MemberManager extends BaseManager<Member, typeof Member> im
     add(ids: string | string[]): Promise<Member> | Promise<Collection<string, Member>> {
         throw new Error('Method not implemented.')
     }
-    remove(member: Member): Promise<this> {
-        throw new Error('Method not implemented.')
+    async remove(member: Member): Promise<FormerMember> {
+        await this.client.rest.api<PostMemberRemoveResponse>(
+            'POST',
+            `groups/${this.group.id}/members/${member.memberID}/remove`,
+        )
+        const formerMember = new FormerMember(this.client, this.group, member.user, member, FormerMemberState.Removed)
+        this.former._upsert(formerMember)
+        this.group.members._remove(member)
+        return formerMember
     }
 }
